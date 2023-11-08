ea4a57e166d23774d4c086c622c58a0709153858
kvaibhav1623 <79585120+kvaibhav1623@users.noreply.github.com>
2023-11-08 12:21:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.random() * 6 + 1;
+randomNumber1 = Math.floor(randomNumber1);
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomeImageSrc = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomeImageSrc);
+
+var randomNumber2 = Math.random() * 6 + 1;
+randomNumber2 = Math.floor(randomNumber2);
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomeImageSrc2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomeImageSrc2);
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if (randomNumber1 === randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "DRAW";
+}
+else{
+    document.querySelector("h1").innerHTML = "Player 2 wins";  
+}
